73fb1934131671ab7df2c0becdd3f60d47245654
Payam Karbassi <pkarbassi@hotmail.co.uk>
2023-05-22 16:40:09 +0100

    4th Commit: Set up login title

--- Appiversity/View/ViewController.swift
+++ Appiversity/View/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    let titleLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Login"
+        label.font = UIFont.systemFont(ofSize: 36, weight: .semibold)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
-        
+        view.backgroundColor = .white
+        setupLoginTitle()
+    }
+    
+    func setupLoginTitle(){
+        view.addSubview(titleLabel)
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
+        titleLabel.centerX(inView: view)
     }
 }
 
